7600dde4c7c71fb73f515ce165743f0ad8baef45
Linus Ericsson <oscarlinusericsson@gmail.com>
2016-12-04 21:16:23 +0100

    Bumped deps

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "http://github.com/levand/prolin"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.apache.commons/commons-math3 "3.2"]]
-  :profiles {:test {:dependencies [[org.clojure/test.generative "0.5.0"]]}}
-  :aliases {"generative" ["run" "-m" "clojure.test.generative.runner" "test"]}
+  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
+                 [org.apache.commons/commons-math3 "3.6.1"]]
+  :profiles {:test {:dependencies [[org.clojure/test.generative "0.5.2"]]}}
+  :aliases {"generative" ["with-profile" "test" "run" "-m" "clojure.test.generative.runner" "test"]}
   :jvm-opts ^:replace ["-Dclojure.test.generative.msec=30000"])
