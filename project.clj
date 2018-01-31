789ac314b199899da7db42b6d539f5a917e7e341
Linus Ericsson <linus.ericsson@agical.se>
2018-01-31 11:11:40 +0100

    upgraded clojure deps

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject prolin "0.1.3-SNAPSHOT"
-  :description "A linear programming library for Clojure"
-  :url "http://github.com/levand/prolin"
+(defproject prolin "0.1.4"
+  :description "A linear programming library for Clojure (forked from Levand)"
+  :url "http://github.com/claj/prolin"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.apache.commons/commons-math3 "3.6.1"]]
   :profiles {:test {:dependencies [[org.clojure/test.generative "0.5.2"]]}}
   :aliases {"generative" ["with-profile" "test" "run" "-m" "clojure.test.generative.runner" "test"]}
